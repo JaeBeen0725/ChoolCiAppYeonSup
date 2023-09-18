511deb576f88d75f7997dbdecc822334a6f67670
github-username <dhwoqls1@gmail.com>
2023-09-18 20:24:47 +0900

    Initial Commit

---
+++ ChoolCiAppYeonSup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChoolCiAppYeonSup
+//
+//  Created by Jae Oh on 2023/09/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
